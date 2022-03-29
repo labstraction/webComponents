8280c24c8d90c146073c3e269299c2398137a54a
labstraction <45163263+labstraction@users.noreply.github.com>
2022-03-29 13:23:45 +0200

    linting

--- super-header.js
+++ super-header.js
@@ -1,41 +1,42 @@
 class SuperHeader extends HTMLHeadingElement {
-  constructor() {
-    super();
-  }
-
-  attributeChangedCallback() {
-    this.getAttributes();
-    this.initTag();
-  }
-
-  static get observedAttributes() { return ['user-count']; }
-
-  getAttributes() {
-    if (this.getAttribute('user-count')) {
-      this.userCount = parseInt(this.getAttribute('user-count'));
-    }
-  }
-
-  initTag() {
-    this.style.background = 'green';
-
-    let container;
-    if (document.getElementById('additional-content')) {
-      container = document.getElementById('additional-content');
-      container.innerHTML = '';
-    } else {
-      container = document.createElement('div');
-      container.id = 'additional-content';
+    // eslint-disable-next-line no-useless-constructor
+    constructor() {
+        super();
     }
 
-    const span = document.createElement('span');
-    const node = document.createTextNode(`count: ${this.userCount}`);
-    span.appendChild(node);
+    attributeChangedCallback() {
+        this.getAttributes();
+        this.initTag();
+    }
 
-    container.appendChild(span);
+    static get observedAttributes() { return ['user-count']; }
 
-    this.appendChild(container);
-  }
+    getAttributes() {
+        if (this.getAttribute('user-count')) {
+            this.userCount = parseInt(this.getAttribute('user-count'), 10);
+        }
+    }
+
+    initTag() {
+        this.style.background = 'green';
+
+        let container;
+        if (document.getElementById('additional-content')) {
+            container = document.getElementById('additional-content');
+            container.innerHTML = '';
+        } else {
+            container = document.createElement('div');
+            container.id = 'additional-content';
+        }
+
+        const span = document.createElement('span');
+        const node = document.createTextNode(`count: ${this.userCount}`);
+        span.appendChild(node);
+
+        container.appendChild(span);
+
+        this.appendChild(container);
+    }
 }
 
 window.customElements.define('super-header', SuperHeader, { extends: 'h1' });
